5a2cdce039c0cf578cdd0a86b38eec896b373a92
Md. Showkat Hossain Chy <showkat2203@users.noreply.github.com>
2022-08-18 15:01:42 -0500

    Time: 433 ms (27.14%), Space: 80.1 MB (39.23%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         
-        int cur_size = 0;
-        
         int sz = arr.size();
         map<int, int> occ_mp;
         
         for(int i = 0; i < sz; i++ )
-        {
             occ_mp[arr[i]]++;
-        }
         
         vector<int> new_vec;
-        
         for(auto it : occ_mp)
         {
             new_vec.push_back(it.second);
-            cur_size += it.second;
         }
         
         sort(new_vec.rbegin(), new_vec.rend());
-        sz = new_vec.size();
 
-    
-        int half = cur_size / 2;
+        int half = sz / 2;
         int sum = 0, res = 0;
-        for(int i = 0; i < sz; i++ )
+        for(int i = 0; i < new_vec.size(); i++ )
         {
             sum += new_vec[i];
             res++;
             if( sum >= half )
                 break;
-            
         }
         
         return res;
