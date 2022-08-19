b9b38fc6b9454f4437bc1c65bedfa0c71d1557b5
Md. Showkat Hossain Chy <showkat2203@users.noreply.github.com>
2022-08-19 03:15:15 -0500

    Time: 488 ms (37.95%), Space: 51.3 MB (19.05%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        if( nums.size() <= 0)
+            return 0;
+        
+        int ans = 0;
+        
+        set<int> freqs;
+        
+        for(int i = 0; i < nums.size(); i++ )
+        {
+            freqs.insert(nums[i]);
+        }
+        
+        auto prev = freqs.begin() ;
+        cout << *prev << endl;
+        int cnt = 1;
+        int last = *prev;
+        int fl = 0, maxi = 0;
+        for(auto it = freqs.begin(); it != freqs.end(); it++ )
+        {
+            if( fl == 0 )
+            {
+                fl = 1;
+                continue;
+            }
+            if( *it == last + 1 )
+            {
+                cnt++;
+                maxi = max(maxi, cnt);
+            }
+            else
+            {
+                cnt = 1;
+            }
+            last = *it;
+            cout << *it << " ";
+        }
+        cout << endl;
+        
+        maxi = max(maxi, cnt);
+        
+        
+          
+        
+        return maxi;
+    }
+};
