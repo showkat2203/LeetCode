9d19816fdc026adcdca7438fd36cab7f4ad5fc36
Md. Showkat Hossain Chy <showkat2203@users.noreply.github.com>
2022-08-21 20:39:38 -0500

    Time: 8 ms (8.64%), Space: 5.9 MB (71.30%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        
+        if( n <= 0)
+            return false;
+        
+        while( n != 1 )
+        {
+            if( n % 4 )
+                return false;
+            n /= 4;
+        }
+        
+        if( n == 1 )
+            return true;
+        return false;
+        
+    }
+};
