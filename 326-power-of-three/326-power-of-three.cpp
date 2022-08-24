39360e1eee06f88dacf6d53f846f004294673046
Md. Showkat Hossain Chy <showkat2203@users.noreply.github.com>
2022-08-23 20:01:21 -0500

    Time: 42 ms (30.41%), Space: 5.7 MB (98.87%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if( n <= 0)
+            return false;
+        
+        while( n != 1 )
+        {
+            if( n % 3 )
+                return false;
+            n /= 3;
+        }
+        
+        if( n == 1 )
+            return true;
+        return false;
+    }
+};
