7a22626794952589d51ef0c5d4151028defe55c4
Md. Showkat Hossain Chy <10063442+showkat2203@users.noreply.github.com>
2024-01-16 23:24:13 -0600

    Time: 196 ms (86.91%), Space: 135 MB (83.10%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,72 +1,22 @@
-// class Solution {
-// public:
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         vector<int> ans;
-        
-//         int lft = 0, rgt = 1;
-//         int sz = nums.size();
-//         int maxi = nums[lft];
-        
-//         dequeu<int> dq;
-        
-//         for(int i = 0; i < k; i++ )
-//             maxi = max(maxi, nums[i]), dq.push_back(nums[i]);
-        
-//         ans.push_back(maxi);
-//         rgt = k;
-        
-//         while( rgt < sz ){
-//             if(nums[rgt] > maxi){
-//                 maxi = max(max, nums[rgt]);
-//                 ans.push_back(maxi);
-//                 rgt++;
-//                 while(!dq.empty() && dq.front() <= maxi)
-//                     dq.pop_front(();
-//                 dq.push_back(maxi);
-//             }
-//             else
-//             {
-//                 int cur = dq.pop_front();
-//                 maxi = max(max, cur);
-//                 while(!dq.empty() && dq.front() <= maxi )
-//                     dq.pop_front();
-            
-//                 maxi = max()
-
-//             }
-//         }
-        
-//         return ans;
-//     }
-// };
-
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        // priority_queue<int, vector<int>, greater<int>> pq;
-        priority_queue<pair<int, int>> pq;
-        for(int i = 0; i < k; i++ )
-        {
-            pq.push({nums[i], i});
+        
+        deque<int> dq;
+        
+        for(int i = 0; i < nums.size(); i++ ){
+            if( dq.front() == i - k )   dq.pop_front();
+            while( !dq.empty() && nums[dq.back()] < nums[i] )
+                    dq.pop_back();
+            
+            dq.push_back(i);
+            
+            if( i - k + 1 >= 0 )
+                ans.push_back(nums[dq.front()]);
         }
-
-        ans.push_back(pq.top().first);
-        cout << pq.top().first << " " << endl;
-
-        int rgt = k, lft = 0;
-        while(rgt < nums.size()){
-
-            while( !pq.empty() && pq.top().second <= (rgt - k) )
-                pq.pop();
-            pq.push({nums[rgt], rgt});
-            rgt++;
-            ans.push_back(pq.top().first);
-            cout << pq.top().first << " " << endl;
-        }
+        
         return ans;
-
     }
-
 };
+
