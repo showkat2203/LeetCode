0a4c752846c4802ad9c88c2d6833be19edeecd73
Md. Showkat Hossain Chy <10063442+showkat2203@users.noreply.github.com>
2024-03-22 03:55:13 -0700

    Time: 27 ms, Memory: 22.8 MB - LeetHub

---
+++ merge-intervals/merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+
+        int low = intervals[0][0], high = intervals[0][1];
+        for(int i = 1; i < intervals.size(); i++ ){
+            if( low <= intervals[i][0] && intervals[i][0] <= high ){
+                high = max(high, intervals[i][1]);
+            }
+            else{
+                ans.push_back({low, high});
+                low = intervals[i][0], high = intervals[i][1];
+            }
+        }
+        ans.push_back({low, high});
+
+        return ans;
+    }
+};
