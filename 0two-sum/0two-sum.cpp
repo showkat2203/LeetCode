9c1069a9b3c1584d3e75c5f18654cbf474ae45a9
Md. Showkat Hossain Chy <10063442+showkat2203@users.noreply.github.com>
2024-03-21 17:52:46 -0700

    Time: 12 ms, Memory: 14.1 MB - LeetHub

---
+++ 0two-sum/0two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++ ){
+            int y = target - nums[i];
+            if( mp.find(y) != mp.end() ){
+                return {i, mp[y]};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
